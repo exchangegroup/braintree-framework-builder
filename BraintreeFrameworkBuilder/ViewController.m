2a86c5e42e9bc0a7a37e5d20266ee31d398ca7d4
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-03-31 17:19:33 +1100

    Use braintree

--- BraintreeFrameworkBuilder/ViewController.m
+++ BraintreeFrameworkBuilder/ViewController.m
@@ -17,7 +17,10 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
+  // will crash with error "BTClient could not initialize because the provided clientToken was invalid"
+  // Which is what we want. It means Braintree SDK is loaded.
+  [Braintree braintreeWithClientToken:@"client_token"];
 }
 
 - (void)didReceiveMemoryWarning {
